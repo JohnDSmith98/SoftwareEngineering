1c8841a77afb170114b135914e9837365b34b4c3
JohnDSmith98 <146352311+JohnDSmith98@users.noreply.github.com>
2023-10-11 18:31:40 +0100

    Did 207 Tasks

--- Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
+++ Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
@@ -2,12 +2,10 @@
 
 int main()
 {
-    //Counter variable
-    int i;
 
 
     //For loop (pre-C99 standard)
-    for (i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)
     {
         //Perform code tasks
         printf("i=%d\n", i);
@@ -15,29 +13,34 @@ int main()
 
     // TASK A - count from 10 down to 0 in steps of -1
 
-    for (i = 10; i > 0; i--)
+    for (int i = 10; i > 0; i--)
     {
         printf("i=%d\n", i);
     }
 
     // TASK B - count from 10 down to -10 in steps of -2
 
-    for (i = 10; i >= -10; i-=2)
+    for (int i = 10; i >= -10; i-=2)
     {
         printf("i=%d\n", i);
     }
 
     // TASK C - count from 10 to 200 in steps of 10
 
-    for (i = 10; i <= 200; i+=10)
+    for (int i = 10; i <= 200; i+=10)
     {
         printf("i=%d\n", i);
     }
 
     // TASK D - count from -10 down to 20 in steps of 5
 
+    for (int i = -10; i >= -20; i -= 5)
+    {
+        printf("i=%d\n", i);
+    }
+
     // TASK E - can you predict the largest value of i that is displayed?
-    for (i = 0; i < 12; i += 3)
+    for (int i = 0; i < 12; i += 3)
     {
         //Perform code tasks
         printf("i=%d\n", i);
