a22b7480cc39e5448680c57ad603c116f61f26af
John Smith <146352311+JohnDSmith98@users.noreply.github.com>
2023-10-19 14:31:55 +0100

    501 Tasks

--- Tasks/05 - Functions/Functions/501-function/501-function.cpp
+++ Tasks/05 - Functions/Functions/501-function/501-function.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int L;		//This is not an ideal location - we will soon address this
 
+void DisplayBanner()
+{
+	for (unsigned int n = 0; n < (L + 4); n++) {
+		cout << "*";
+	}
+	cout << endl;
+}
+
 int main()
 {
 	const string message = "Welcome to Computer Science";
@@ -10,21 +18,13 @@ int main()
 	//Get the length of the string
 	L = message.length();
 
-	//Write the banner above
-	for (unsigned int n = 0; n < (L + 4); n++) {
-		cout << "*";
-	}
-	cout << endl;
+	DisplayBanner();
 
-	cout << "* ";				//Leading *
+	cout << ". ";				//Leading *
 	cout << message;			//Message
-	cout << " *" << endl;		//Trailing *
+	cout << " ." << endl;		//Trailing *
 
-	//Write the banner below
-	for (unsigned int n = 0; n < (L + 4); n++) {
-		cout << "*";
-	}
-	cout << endl;
+	DisplayBanner();
 
 	//Tell the calling shell all is well
 	return 0;
